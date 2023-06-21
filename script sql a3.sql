70cf84241bf5c93b029e0ee429b31023a3b22626
Gabriel Pastore <79768975+GaPastore@users.noreply.github.com>
2023-06-21 12:56:32 -0300

    Versão Final do Projeto
    Esta é a versão final do projeto, depois das atualizações, correções e melhorias visuais possíveis.

--- script sql a3.sql
+++ script sql a3.sql
@@ -2,15 +2,17 @@ create database db_sistema_cadastro;
 use db_sistema_cadastro;
 
 drop table tb_usuario;
+drop table tb_empresa;
 
 create table tb_usuario (
-    id int auto_increment, 
-    constraint pk_usuario
-    primary key (id),
+    id int primary key auto_increment,
+    new_data varchar(10),
     nome varchar(50),
     email varchar(50),
     senha varchar(20),
     endereco varchar(45),
+    cpf varchar(11),
+    cnpj varchar(14),
     bairro varchar(30),
     cidade varchar(20),
     estado varchar(2),
@@ -20,18 +22,14 @@ create table tb_usuario (
     tel_come varchar(10), #sem formatação
     cel_come varchar(11), #sem formatação
     comple varchar(15),
-    tipo_usuario int(1)
+    tipo_usuario int
     );
-
-drop table tb_empresa;
-
-create table tb_empresa (
-    id int auto_increment, 
-    constraint pk_empresa
-    primary key (id),
-    id_cli int,
-    bairro_emp varchar(30),
+ create table tb_empresa (
+	id int primary key auto_increment,
+	bairro_emp varchar(30),
     cidade_emp varchar(20),
+    endereco_emp varchar(45),
+    email_emp varchar(50),
     estado_emp varchar(2),
     cep_emp varchar(8),
     cnpj_emp varchar(14),
@@ -39,17 +37,11 @@ create table tb_empresa (
     cel_emp varchar(8),
     nome_emp varchar(50),
     comp_emp varchar(15),
-    arq_emp varchar(50)
-);
-
-alter table tb_empresa 
-add constraint fk_empresausuario 
-foreign key (id_cli) references tb_usuario(id);
-
-insert into tb_usuario (nome, senha) values ('admin', 'admin');
+    arq_emp varchar(50),
+    aprovado int,
+    id_usuario int,
+    CONSTRAINT fk_usuario_empresa FOREIGN KEY (id_usuario) REFERENCES tb_usuario (id)
+    );
 
 select * from tb_usuario;
 select * from tb_empresa;
-
-select nome from tb_usuario;
-
